d4bb5990849fe58b587ba43ad25f2aa1640d8189
fadezak100 <fadyzaqut@gmail.com>
2022-09-11 00:01:29 +0300

    fix auth issue for comments in the landing page #relates 44

--- src/routers/comments.js
+++ src/routers/comments.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { getAllPostComments, insertNewComment } = require('../controllers');
 const verifyAccessToken = require('../middlewears/verifyAccessToken');
 
-router.get('/:postId', verifyAccessToken, getAllPostComments);
+router.get('/:postId', getAllPostComments);
 router.post('/new-comment', verifyAccessToken, insertNewComment);
 
 module.exports = router;
